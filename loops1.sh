869f08fff201018bdae25eb6fe127684f6d943a0
Siarhei Saroka <siarhei_saroka@epam.com>
2022-12-14 21:01:59 +0300

    string manipulations done

--- loops1.sh
+++ loops1.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+arg1=$1
 
-# Put your code here
+if [[ -z $1 ]]; then 
+    echo "no param"
+    exit 1
+fi
+
+length=${#arg1}
+res=""
+
+while [[ $length -gt 0 ]]; do
+    ((length-=1))
+    res+="${arg1:length:1}"
+done
+echo "$res"| tr 'A-Za-z' 'a-zA-Z'
+
+
+
+
+
